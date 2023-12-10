3fe782bb59ee41c243e347c946d90c2e66378f96
electro-coder <ee22b060@iittp.ac.in>
2023-12-10 17:10:16 +0530

    task.py updated, main.py updated

--- Main/main.py
+++ Main/main.py
@@ -1,1 +1,27 @@
-# Contains the wrapper class 
+# Wrapper Class
+
+from tkinter import *
+from tkinter import filedialog
+import csv
+
+# Teams' main file import
+from Team_0 import team_0
+
+
+class wrapper:
+
+    def run(self):
+        filepath = filedialog.askopenfilename()
+        acronyms = 0  # number of acronyms
+        acronym_word = []  # creating a list to store acronyms found in the file
+        raw_data = []
+        with open(filepath,'r',errors='ignore') as file:
+            text=str(file.read())
+
+        # Calling all team run() files
+        obj_team_0 = team_0(text)
+        output_team_0 = obj_team_0.run()
+
+
+obj = wrapper()
+obj.run()
