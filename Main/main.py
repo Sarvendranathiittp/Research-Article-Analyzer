f78be2d39ac72f0a2df06effb2578696db58a49c
electro-coder <ee22b060@iittp.ac.in>
2023-12-11 07:46:41 +0530

    recommended changes made, sample log file created

--- Main/main.py
+++ Main/main.py
@@ -2,7 +2,6 @@
 
 from tkinter import *
 from tkinter import filedialog
-import csv
 
 # Teams' main file import
 from Team_0.task import team_0
@@ -12,23 +11,26 @@ class wrapper:
 
     def run(self):
         filepath = filedialog.askopenfilename()
-        acronyms = 0  # number of acronyms
-        acronym_word = []  # creating a list to store acronyms found in the file
         raw_data = []
+        output = []
+
         with open(filepath,'r',errors='ignore') as file:
             text=str(file.read())
+        begin_index = text.find(r'\begin{document}')
 
         # Calling all team run() files
-        obj_team_0 = team_0(text)
-        output_team_0 = obj_team_0.run()
+        obj_team_0 = team_0(text,begin_index)
+        output.append(obj_team_0.run())
 
 
 
 
         # Writing all the output logs to the log file
         with open ("LOGII", "a") as logw:
-            for log in output_team_0:
-                logw.write(log)
+            for logs in output:
+                for log in logs:
+                    logw.write(log)
+                print()
 
 obj = wrapper()
 obj.run()
