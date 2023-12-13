d070827f6c4e11f333e8924669ec773baa6b9353
Anirudhan <anirudhans2004@gmail.com>
2023-12-13 14:17:44 +0530

    updated acronyms

--- Main/Team_3/task.py
+++ Main/Team_3/task.py
@@ -19,9 +19,10 @@ class team_3:
         str = '='*50
         output.append(str+'\n\t\t\t\t\t\tScientist Names Related Comments\n'+str)
         self.scientistName(text,output)
-        output.append(str+'\n\t\t\t\t\t\tIndex Related Comments\n'+str)
+        output.append(str+'\n\t\t\t\t\t\tIndex Related Comments\n'+str+'\n')
         output.extend(self.indexCheck())
-        
+        output.append('\n'+str+'\n\t\t\t\t\t\tAcronym Related Comments\n'+str)
+        self.acron(text,output)
         
         return output
     
@@ -29,64 +30,51 @@ class team_3:
         location=dict()
         #set to avoid repeatition
         scientist_names_used=set()
-        scientist_names_used1=set()
-        scientist_names2=[]
         #List is used for ordering       
-        scientist_names=[['Isaac Newton','Newton'],['Albert Einstein','Einstein'],'Galileo Galilei',['Niels Bohr','Bohr'],'Marie Curie',
-                          ['Max Planck','Planck'],['James Clerk Maxwell','Maxwell'],['Werner Heisenberg','Heisenberg'],['Richard Feynman','Feynman'],
-                          ['Erwin Schrödinger','Schrödinger'],'Enrico Fermi','Stephen Hawking','Hawking',['Michael Faraday',
-                         'Faraday'],'Dmitri Mendeleev','Carl Sagan','Andrei Sakharov','Lise Meitner',
+        scientist_names=['Isaac Newton','Newton','Albert Einstein','Einstein','Galileo Galilei','Niels Bohr','Bohr','Marie Curie',
+                          'Max Planck','Planck','James Clerk Maxwell','Maxwell','Werner Heisenberg','Heisenberg','Richard Feynman',
+                         'Feynman','Erwin Schrödinger','Schrödinger','Enrico Fermi','Stephen Hawking','Hawking','Michael Faraday',
+                         'Faraday','Dmitri Mendeleev','Carl Sagan','Andrei Sakharov','Lise Meitner',
                          'Edwin Hubble','Jocelyn Bell Burnell','Chandrasekhar Subrahmanyan',
                          'Gaussian','Doppler']
-        
-        for element in scientist_names:
-            if isinstance(element, list):
-                scientist_names2.extend(element)
-            else:
-                scientist_names2.append(element)
-        #scientist_names2con
         #scientist_names 1 contains all the scientist names in lower case
-        scientist_names1 = [string.lower() for string in scientist_names2]
+        scientist_names1 = [string.lower() for string in scientist_names]
 
         #text1 contains all the latex code in lower case
         text1 = text.lower()
 
-        """If the scientist names are present in the text it will add 
-        its location in the dictionary along with the real word"""
-        for word,realword in zip(scientist_names1,scientist_names2):
+        for word,realword in zip(scientist_names1,scientist_names):
             if text1.find(word)!=-1:
                 location[text1.find(word)]=realword
                 #output.append(str(text1.find(word))+" "+realword)
-                scientist_names_used.add(text[text1.find(word):text1.find(word)+len(word)])
+                scientist_names_used.add(realword)
                 
         for key,value in location.items():
             if text[key:key+len(value)]!=value:
                 output.append(text[key:key+len(value)]+" is not in proper format ")
-        
-        for element in scientist_names:
-                # Check if the element is a list
-            if isinstance(element, list):
-                # Check if any element in the sublist is present in S1
-                if any(sub_element in scientist_names_used for sub_element in element):
-                    # Add the first element of the sublist to the set
-                    scientist_names_used1.add(element[0])
-            else:
-                # Check if the standalone element is present in S1
-                if element in scientist_names_used:
-                    # Add the standalone element to the set
-                    scientist_names_used1.add(element)
 
+
+
+
+        """for word in scientist_names:
+            if text.find(word)!=-1:
+                scientist_names_used.add(word)
+
+        for word in text.split(' '):
+            if word.lower() in scientist_names:
+                scientist_names_used.add(word)"""
         str1=''
-        for word in scientist_names_used1:
+        for word in scientist_names_used:
             if str1=='':
                 str1=word
             else:
                 str1=str1+', '+word
+        output.append('\nScientist Names Used = '+str1+'\n')
         for word in scientist_names_used:
             if word[0].islower() is True: # can directly check True/False is True not required
                 output.append(word+" should start with a capital letter as it is a proper name ")
         
-        output.append('\nScientist Names Used = '+str1+'\n')
+            
     """
     Make seperate functions for whatever you do and call it in run
     """
@@ -100,14 +88,14 @@ class team_3:
         text2=text1.replace("."," ")
         for word in text2.split(' '):
             if len(word)>1:
-                if word[0:2].isupper():  
+                x=sum(1 for c in word if c.isupper())
+                if(x>=2):
                     index = len(acronym_word)
                     acronym_word.insert(index,word)
             else:
                 pass
-        uniq=sorted(set(acronym_word))
         unique_words = []
-        for word in uniq:
+        for word in acronym_word:
             if word not in unique_words:
                 output.append("\n"+word)
         
