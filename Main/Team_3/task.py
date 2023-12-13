47afe82e179a9c8fc9bb0e9cbcf2101e821f68b3
Akileshdash <119668304+Akileshdash@users.noreply.github.com>
2023-12-13 13:11:08 +0530

    Scientist Names Updated from signle word recognition to multiple words

--- Main/Team_3/task.py
+++ Main/Team_3/task.py
@@ -17,6 +17,7 @@ class team_3:
         text = self.latex_code
         # use self
         str = '='*50
+        output.append(str+'\n\t\t\t\t\t\tScientist Names Related Comments\n'+str)
         self.scientistName(text,output)
         output.append(str+'\n\t\t\t\t\t\tIndex Related Comments\n'+str)
         output.extend(self.indexCheck())
@@ -25,24 +26,49 @@ class team_3:
         return output
     
     def scientistName(self,text,output):
+        location=dict()
+        #set to avoid repeatition
         scientist_names_used=set()
-        scientist_names={'Isaac Newton','Newton','Albert Einstein','Einstein','Galileo Galilei','Niels Bohr','Bohr','Marie Curie',
-                         'Max Planck','Planck','James Clerk Maxwell','Maxwell','Werner Heisenberg','Heisenberg','Richard Feynman',
+        #List is used for ordering       
+        scientist_names=['Isaac Newton','Newton','Albert Einstein','Einstein','Galileo Galilei','Niels Bohr','Bohr','Marie Curie',
+                          'Max Planck','Planck','James Clerk Maxwell','Maxwell','Werner Heisenberg','Heisenberg','Richard Feynman',
                          'Feynman','Erwin Schrödinger','Schrödinger','Enrico Fermi','Stephen Hawking','Hawking','Michael Faraday',
                          'Faraday','Dmitri Mendeleev','Carl Sagan','Andrei Sakharov','Lise Meitner',
                          'Edwin Hubble','Jocelyn Bell Burnell','Chandrasekhar Subrahmanyan',
-                         'Gaussian','Doppler'}
-        scientist_names = {string.lower() for string in scientist_names}
+                         'Gaussian','Doppler']
+        #scientist_names 1 contains all the scientist names in lower case
+        scientist_names1 = [string.lower() for string in scientist_names]
+
+        #text1 contains all the latex code in lower case
+        text1 = text.lower()
+
+        for word,realword in zip(scientist_names1,scientist_names):
+            if text1.find(word)!=-1:
+                location[text1.find(word)]=realword
+                #output.append(str(text1.find(word))+" "+realword)
+                scientist_names_used.add(realword)
+                
+        for key,value in location.items():
+            if text[key:key+len(value)]!=value:
+                output.append(text[key:key+len(value)]+" is not in proper format ")
+
+
+
+
+        """for word in scientist_names:
+            if text.find(word)!=-1:
+                scientist_names_used.add(word)
+
         for word in text.split(' '):
             if word.lower() in scientist_names:
-                scientist_names_used.add(word)
+                scientist_names_used.add(word)"""
         str1=''
         for word in scientist_names_used:
             if str1=='':
                 str1=word
             else:
                 str1=str1+', '+word
-        output.append('Scientist Names Used = '+str1+'\n')
+        output.append('\nScientist Names Used = '+str1+'\n')
         for word in scientist_names_used:
             if word[0].islower() is True:
                 output.append(word+" should start with a capital letter as it is a proper name ")
