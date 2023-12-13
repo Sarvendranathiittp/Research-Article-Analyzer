6767be911fa1ef443d7d383f2c34c94478fc1ead
SaiMadhavChakrala <saimadhavchakrala05@gmail.com>
2023-12-13 15:50:18 +0530

    Extending the function that generates a list containing starting and ending indices of all the equations for various cases

--- Main/Team_7/zero.py
+++ Main/Team_7/zero.py
@@ -4,11 +4,14 @@ class zero:
         self.begin_index = begin_index
     def run(self):
         dotindex =[]
+        equations_=self.get_inline_equations()
         for i in range(self.begin_index,len(self.code)):
             if (self.code(i) == '.') & ((self.code(i + 1) <= '9') & (self.code(i + 1) >= '0')):
                 dotindex.append(i)
 
+
     def get_inline_equations(self):
+    # A function that generates a list containing starting and ending indices of all the equations
         equations = []
         found = False
         current_equation = []
@@ -37,7 +40,93 @@ class zero:
                 equations.append(current_equation)
                 found = False
                 current_equation = []
+            elif not found and self.code[i:i + len("\\begin{equation}")] == "\\begin{equation}" :
+                current_equation.append(i)
+                found = True
+            elif found and self.code[i:i + len("\\end{equation")] == "\\begin{equation}" :
+                current_equation.append(i)
+                equations.append(current_equation)
+                found = False
+                current_equation = []
+            elif not found and self.code[i:i + len("\\begin{align}")] == "\\begin{align}" :
+                current_equation.append(i)
+                found = True
+            elif found and self.code[i:i + len("\\end{align}")] =="\\end{align}" :
+                current_equation.append(i)
+                equations.append(current_equation)
+                found = False
+                current_equation = []
+            elif not found and self.code[i:i + len("\\begin{align*}")] == "\\begin{align*}" :
+                current_equation.append(i)
+                found = True
+            elif found and self.code[i:i + len("\\end{align*}")] =="\\end{align*}" :
+                current_equation.append(i)
+                equations.append(current_equation)
+                found = False
+                current_equation = []
+            elif not found and self.code[i:i + len("\\begin{bmatrix}")] == "\\begin{bmatrix}" :
+                current_equation.append(i)
+                found = True
+            elif found and self.code[i:i + len("\\end{bmatrix}")] =="\\end{bmatrix}" :
+                current_equation.append(i)
+                equations.append(current_equation)
+                found = False
+                current_equation = []
+            elif not found and self.code[i:i + len("\\begin{multiline}")] == "\\begin{multiline}" :
+                current_equation.append(i)
+                found = True
+            elif found and self.code[i:i + len("\\end{multiline}")] =="\\end{multiline}" :
+                current_equation.append(i)
+                equations.append(current_equation)
+                found = False
+                current_equation = []
+            elif not found and self.code[i:i + len("\\begin{multiline*}")] == "\\begin{multiline*}" :
+                current_equation.append(i)
+                found = True
+            elif found and self.code[i:i + len("\\end{multiline*}")] =="\\end{multiline*}" :
+                current_equation.append(i)
+                equations.append(current_equation)
+                found = False
+                current_equation = []
+            elif not found and self.code[i:i + len("\\begin{gather}")] == "\\begin{gather}" :
+                current_equation.append(i)
+                found = True
+            elif found and self.code[i:i + len("\\end{gather}")] =="\\end{gather}" :
+                current_equation.append(i)
+                equations.append(current_equation)
+                found = False
+                current_equation = []
+            elif not found and self.code[i:i + len("\\begin{gather*}")] == "\\begin{gather*}" :
+                current_equation.append(i)
+                found = True
+            elif found and self.code[i:i + len("\\end{gather*}")] =="\\end{gather*}" :
+                current_equation.append(i)
+                equations.append(current_equation)
+                found = False
+                current_equation = []
+            elif not found and self.code[i:i + len("\\begin{tabular}")] == "\\begin{tabular}" :
+                current_equation.append(i)
+                found = True
+            elif found and self.code[i:i + len("\\end{tabular}")] =="\\end{tabular}" :
+                current_equation.append(i)
+                equations.append(current_equation)
+                found = False
+                current_equation = []
+            elif not found and self.code[i:i + len("\\begin{document}")] == "\\begin{document}" :
+                current_equation.append(i)
+                found = True
+            elif found and self.code[i:i + len("\\end{document}")] =="\\end{document}" :
+                current_equation.append(i)
+                equations.append(current_equation)
+                found = False
+                current_equation = []
+
+
+
+
         return equations
+
+
 
 
 
