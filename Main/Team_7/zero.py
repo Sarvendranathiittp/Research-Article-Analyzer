491926d6441ece21457c7ee994471351971303b5
Aswin Arun <153053380+Aswin-Arun@users.noreply.github.com>
2023-12-14 16:37:43 +0530

    Added a getline function to find the line where an index occurs

--- Main/Team_7/zero.py
+++ Main/Team_7/zero.py
@@ -2,6 +2,7 @@ class zero:
     def __init__(self,code,begin_index):
         self.code = code
         self.begin_index = begin_index
+
     def run(self):
         dotindex =[]
         for i in range(self.begin_index,len(self.code)):
