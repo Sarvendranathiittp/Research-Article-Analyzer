fe6d15b1d437961ce83e64a9f0d68af50f64c9e5
Mokshith1708 <146767844+Mokshith1708@users.noreply.github.com>
2023-12-22 19:12:15 +0530

    Added code for checking Comma for all intermediate the equations. here whenMultiple equations are written using align

--- Main/Team_5/task.py
+++ Main/Team_5/task.py
@@ -5,6 +5,50 @@ class team_5:
         self.latex_code = latex_code
         self.text_begin = text_begin
     
+    def check_punctuation_between_multiequations(self,latex_content):
+        self.latex_content=latex_content
+        result = []
+        punc = False
+        char_next = False
+        equation_pattern = re.compile(r'\\begin{align}(.*?)\\end{align}',re.DOTALL)
+        equations = re.findall(equation_pattern, latex_content)
+        
+        for equation in equations:
+            for i in range(0,len(equation) - 1):
+                if equation[i]=='\\' and equation[i+1]=='\\':
+                    
+                    for j in range(i - 1, -1, -1):
+                        if equation[j] != ' ':  # If the character is not a space
+                            if equation[j]==',':
+                                punc = True
+                                # print("1")
+                                # print(equation)
+                            break
+                    
+                    for k in range(i+2,i+10):
+                        # print(equation[j])
+                        if equation[k] not in [' ', '\n']:
+                            if equation[k]=="\\":  
+                                char_next = True
+                                # print(equation[k])
+                            if punc==True and equation[k]=="&":
+                                char_next = True
+                            break
+                # if char_next and punc:
+                #     print("yes")
+                #     print(equation)
+                if punc and (not char_next):
+                    # print("hi")
+                    # print(equation)
+                    result.append("here , is placed at the end of one of the equation, but it is not expexted there. The equation is  "+equation+"\n")
+                if char_next and (not punc):
+                    # print("bye")
+                    # print(equation)
+                    result.append("here , is missing at the end of one of the equation. The equation is  "+equation+"\n")
+                char_next=False
+                punc=False   
+        return result;                  
+                                
     def check_punctuation(self,latex_content):
         self.latex_content = latex_content
             # Define a regular expression to match LaTeX equations and labels
@@ -63,6 +107,7 @@ class team_5:
                 # print("Warning: Punctuation not found at the end of the equation (if any).")
                 result.append("Warning: Punctuation not found at the end of the equation."+equation+"\n")
         return result  
+    
     def check_punctuation_align(self,latex_content):
         self.latex_content = latex_content
             # Define a regular expression to match LaTeX equations and labels
@@ -121,12 +166,17 @@ class team_5:
                 # print("Warning: Punctuation not found at the end of the equation (if any).")
                 result.append("Warning: Punctuation not found at the end of the equation."+equation+"\n")
         return result          
+    
     def run(self):  # The function which is going to be invoked in the wrapper class should contain no arguments
 
         output = [] # The output list with the errors to be returned
         text = self.latex_code
-        result=self.check_punctuation(text) 
-        r=self.check_punctuation_align(text)    
-        output = result
-        output.extend(r)    
+        result1=self.check_punctuation(text) 
+        result2=self.check_punctuation_align(text) 
+        result3=self.check_punctuation_between_multiequations(text) 
+        start="\n\n ///////////////////####punctuation at the end of equations related comments####\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n"
+        output.extend(start)
+        output.extend(result1)
+        output.extend(result2)    
+        output.extend(result3)
         return output
