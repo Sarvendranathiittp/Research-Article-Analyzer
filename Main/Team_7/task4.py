ec41831bf17aabf902b5fd9a3730a9120e8af2c8
Aswin Arun <153053380+aswin-arun@users.noreply.github.com>
2023-12-22 12:01:02 +0530

    Added more cases and fail cases for task_4

--- Main/Team_7/task4.py
+++ Main/Team_7/task4.py
@@ -1,42 +1,52 @@
 from Main.Team_7.zero import zero
 
 
-class task_4:
-    def __init__(self,code,begin_index):
+class Task_4:
+    def __init__(self, code, begin_index):
         self.code = code
         self.begin_index = begin_index
 
+    @property
     def run(self):
         z = zero(self.code, self.begin_index)
-        equations = z.display_equations()+z.get_inline_equations()
+        equations = z.display_equations() + z.get_inline_equations()
         error_index = []
         for eq in equations:
             comma_status = False
             for i in range(eq[0], eq[1]):
+                if self.code[i] == '.' & (not comma_status):
+                    break
                 if self.code[i] == ',':
                     comma_status = True
                 if (self.code[i: len("\\case")] == "\\case") & (not comma_status):
-                    j = i+len("\\case")
+                    j = i + len("\\case")
                     forall = False
-                    while not (self.code[j: len("\\\\")] == "\\\\"):
+                    while not (self.code[j: len("\\\\")] == "\\\\" & self.code[
+                                                                     j: len("\\newline")] == "\\newline" & self.code[
+                                                                                                           j: len("\\linebreak")] == "\\linebreak"):
                         if self.code[j: len("\\forall")] == "\\forall":
                             forall = True
                             break
                     if not forall:
                         error_index.append(i)
-                if (self.code[i: len("\\text")] == "\\text") & (not comma_status):
+                elif (self.code[i: len("\\text")] == "\\text") & (not comma_status):
                     j = i + len("\\text")
                     forall = False
-                    while not (self.code[j: len("\\\\")] == "\\\\"):
+                    while not (self.code[j: len("\\\\")] == "\\\\" & self.code[
+                                                                     j: len("\\newline")] == "\\newline" & self.code[
+                                                                                                           j: len("\\linebreak")] == "\\linebreak"):
                         if self.code[j: len("\\forall")] == "\\forall":
                             forall = True
                             break
+
                     if not forall:
                         error_index.append(i)
-                if (self.code[i: len("\\mbox")] == "\\mbox") & (not comma_status):
+                elif (self.code[i: len("\\mbox")] == "\\mbox") & (not comma_status):
                     j = i + len("\\mbox")
                     forall = False
-                    while not (self.code[j: len("\\\\")] == "\\\\"):
+                    while not (self.code[j: len("\\\\")] == "\\\\" & self.code[
+                                                                     j: len("\\newline")] == "\\newline" & self.code[
+                                                                                                           j: len("\\linebreak")] == "\\linebreak"):
                         if self.code[j: len("\\forall")] == "\\forall":
                             forall = True
                             break
@@ -44,11 +54,3 @@ class task_4:
                         error_index.append(i)
 
         return error_index
-
-
-
-
-
-
-
-
