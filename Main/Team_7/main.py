b0aa67223f2a6d29b79bab3b8bfc487e80b45993
Aswin-Arun <aswina2702@gmail.com>
2024-03-09 22:05:30 +0530

    Changed Output format

--- Main/Team_7/main.py
+++ Main/Team_7/main.py
@@ -25,13 +25,14 @@ class team_7:
         t2 = Task_2(self.latex_code,self.text_begin)
         s2 = t2.run()
         error_string=[]
-        error_string =error_string + self.create_error_msg(s1,"Leading zero error")
-        error_string = error_string + self.create_error_msg(s1_, "Trailing zero error")
-        error_string = error_string + self.create_error_msg(s2, "Sentence starting with number warning")
-        error_string = error_string + self.create_error_msg(s3, "Extra Parenthesis warning")
-        error_string = error_string + self.create_error_msg(s3_, "Long Exponential expression warning")
-        error_string = error_string + self.create_error_msg(s4, "No comma before condition warning")
-        error_string = error_string + self.create_error_msg(s4_, "No required space at expression and condition")
+        error_string = error_string + ['='*50 + "\n"]
+        error_string =error_string + self.create_error_msg(s1,"Leading zero error.")
+        error_string = error_string + self.create_error_msg(s1_, "Trailing zero error.")
+        error_string = error_string + self.create_error_msg(s2, "Sentence starting with number warning.")
+        error_string = error_string + self.create_error_msg(s3, "Extra Parenthesis warning.")
+        error_string = error_string + self.create_error_msg(s3_, "Long Exponential expression warning.")
+        error_string = error_string + self.create_error_msg(s4, "No comma before condition warning.")
+        error_string = error_string + self.create_error_msg(s4_, "No required space at expression and condition.")
         str= ''
         pruned_error_str = []
         for i in error_string:
@@ -51,9 +52,35 @@ class team_7:
     def create_error_msg(self,error_index_list, error_type):
         string_list = []
         for i in error_index_list:
-            error_string = f"{error_type} at line {self.get_line(i)}"
-            string_list.append(error_string)
+            if not self.is_in_comment(i):
+                error_string = f"Line {self.get_line(i)}: {error_type}\n"
+                string_list.append(error_string)
         return string_list
+
+    def get_comment_list(self):
+        is_in_comment = False
+        comment_list = []
+        comment_indices = []
+        for i in range(self.n):
+            if self.latex_code[i] == '%' and not is_in_comment:
+                is_in_comment = True
+                comment_indices.append(i)
+            elif self.latex_code[i] == '\n' and is_in_comment:
+                is_in_comment = False
+                comment_indices.append(i)
+                comment_list.append(comment_indices)
+                comment_indices = []
+        return comment_list
+
+    def is_in_comment(self, index):
+        in_comment = False
+        c_list = self.get_comment_list()
+        for i in c_list:
+            if i[0]<=index<=i[1]:
+                in_comment = True
+        return in_comment
+
+
 
 
 
